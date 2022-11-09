ce33a640a30a5173f1758cc66e10e7d5e88baac3
Mykola Saliuk <haker0502@ukr.net>
2022-11-10 01:07:20 +0200

    додає код 1 завдання

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const bodyEl = document.querySelector('body');
+let timerId = null;
 
+startBtn.addEventListener('click', onStartBtnClick);
+stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick() {
+  timerId = setInterval(() => {
+    console.log(`Interval with id ${timerId} has started!`);
+    const bodyColor = getRandomHexColor();
+
+    // bodyEl.style.backgroundColor = bodyColor;
+    bodyEl.setAttribute('style', `background-color: ${bodyColor}`);
+  }, 1000);
+
+  startBtn.setAttribute('disabled', true);
+  stopBtn.removeAttribute('disabled');
+}
+
+function onStopBtnClick() {
+  clearInterval(timerId);
+
+  console.log(`Interval with id ${timerId} has stopped!`);
+
+  startBtn.removeAttribute('disabled');
+  stopBtn.setAttribute('disabled', true);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
